b7b326f992f8088de14c11b2b806b650d92a7364
iago0002 <iago.santos@aluno.faculadadeimpacta.com.br>
2020-10-05 22:45:38 -0300

    ac3

--- t8.py
+++ t8.py
@@ -45,8 +45,6 @@ def nao_entre_em_panico():
 
     return primos
 
-    primos = "Tudo vai dar certo caros alunos!"
-
 if __name__ == "__main__":
     port = int(os.environ.get("port", 5000))
     app.run(host='0.0.0.0', port=port)
